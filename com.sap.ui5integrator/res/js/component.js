4a416c30a1d70d3d84e65c3ee885d0b95e9979e6
pramodlumburu <pramod.lpk@gmail.com>
2017-05-18 12:48:43 -0400

    Add files via upload

--- com.sap.ui5integrator/res/js/component.js
+++ com.sap.ui5integrator/res/js/component.js
@@ -10,40 +10,41 @@ define(["sap/designstudio/sdk/component", "css!../css/component.css"], function(
 			this._parm1 = undefined;
 			
 			this._parm1Changed = true;
-		//	this.url = "http://localhost:56653";
+		   this._url = "*";
 			
 
 
 		this.init = function() {
 			
 			this._parm1Changed = true;
-			window.alert('Custom component intialization called ');
-			
+     console.log('Custom component intialization called ');
+			   this._url = "*";
+
 			
 			 function receiveMessage(e) 
 			 { 
 				 
-		//		 console.log(that.url); console.log(e.origin);
-			//	  if (e.origin !==  that.url)
-				//	    return;
-				 
+		 console.log(that._url); 
+		 console.log(e.origin);
+//	     if (e.origin !==  that._url && e.origin !== "*")
+//			    return;
+     //   Console.log( 'Trusted the origin website ' + e.origin) ;  				
 			 var mes = "Message Received at window	: "; 
-			window.alert( mes +  JSON.stringify(e.data) ) ;
+			 console.log( mes +  JSON.stringify(e.data) ) ;
   
 			 that.parm1(e.data ); 
 			 console.log( " window addEventListner called and setting that._parm1 variable to "  + that._parm1 );
 			  
 			 that.firePropertiesChanged(["parm1"]);
-			 that.fireEvent("onclick");
+			 that.fireEvent("onreloadrequest");
+			 that.fireEvent("onparameterschanged");
 
 			 } // end of Callback function for Window Message event listner 
 	
 			
 			 window.addEventListener('message', receiveMessage);
 		
-			this.$().click(function() {
-				that.fireEvent("onclick");
-			});
+		
 		};
 		
 		
@@ -75,18 +76,36 @@ define(["sap/designstudio/sdk/component", "css!../css/component.css"], function(
 		
 		this.parm1 = function(value) {
 		if (value === undefined) {
-				window.alert( " Getter Function :   : "+ this._parm1);
-				return this._parm1;
+				//window.alert( " Getter Function :   : "+ this._parm1);
+			console.log( " Getter Function :   : "+ this._parm1);
+			return this._parm1;
 			} else 
 			
 			{
 					this._parm1 =  JSON.stringify(value);
 					this._parm1Changed = true;
-					window.alert( "  Setter Function assigning to _parm1 : "+ this._parm1 );
+					//window.alert( "  Setter Function assigning to _parm1 : "+ this._parm1 );
+					console.log( "  Setter Function assigning to _parm1 : "+ this._parm1 );
+
 					return this ; 
 					 }
 		 
 		};
+		
+		this.url = function(value) {
+			if (value === undefined) {
+				 console.log( " Getter Function  URL : "+ this._url);
+
+					return this._url;
+				} else 
+				
+				{
+						this._url =  value;
+						console.log( " settter Function  URL   : "+ this._url);
+						return this ; 
+						 }
+			 
+			};
 		
 		
 		
