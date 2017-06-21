0532cfd5a743bb5b73b21ae775840339437c8e1d
pramodlumburu <pramod.lpk@gmail.com>
2017-06-21 15:26:50 -0400

    Add files via upload

--- com.sap.ui5integrator/res/js/component.js
+++ com.sap.ui5integrator/res/js/component.js
@@ -48,44 +48,24 @@ define(["sap/designstudio/sdk/component", "css!../css/component.css"], function(
 		};
 		
 		
-//		this.beforeUpdate = function() {
-//			
-//			
-//			 function receiveMessage(e) 
-//			 { 
-//
-//			 // Update the div element to display the message.
-//			 var mes = "Message Received: " + e.data; 
-//			 window.alert("before  update" + JSON.stringify(e.data));
-//			// window.alert( JSON.stringify(e.data) ) ;
-//			 gCustomer = e.data;
-//
-//			 }
-//			 window.addEventListener('message', receiveMessage);
-//			
-//			
-//	window.alert( "  before Update assigning gCustomer to _param1 : "+ gCustomer+  " value to param1 ");
-//						this.__parm1 = gCustomer;
-//						  return this ; 
-//		
-//			this.$().click(function() {
-//				that.fireEvent("onclick");
-//			});
-//		};
+
 		
 		
 		this.parm1 = function(value) {
 		if (value === undefined) {
 				//window.alert( " Getter Function :   : "+ this._parm1);
-			console.log( " Getter Function :   : "+ this._parm1);
+			console.log( " Getter Function :   : ");
+			console.log(this._parm1);
+
 			return this._parm1;
 			} else 
 			
 			{
-					this._parm1 =  JSON.stringify(value);
+					this._parm1 =  value;
 					this._parm1Changed = true;
 					//window.alert( "  Setter Function assigning to _parm1 : "+ this._parm1 );
-					console.log( "  Setter Function assigning to _parm1 : "+ this._parm1 );
+					console.log( "  Setter Function assigning to _parm1 : " );
+					console.log(this._parm1);
 
 					return this ; 
 					 }
